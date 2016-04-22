f89fdd71935db4cd7e08bd69d6c35433ca35a508
Ádám T. Nagy <tnagy.adam@gmail.com>
2016-04-22 20:58:45 +0200

    Cleaning up and improving variable naming (I think distance is the thing between 2 numbers(days) we are talking about here)

--- src/main/java/org/jfree/date/SerialDate.java
+++ src/main/java/org/jfree/date/SerialDate.java
@@ -714,30 +714,15 @@ public abstract class SerialDate implements Comparable,
         // find the date...
         final int baseDOW = base.getDayOfWeek();
 
-//        int delta = targetDOW - base.getDayOfWeek();
-//        int positiveDelta = delta + 7;
-//        int adjust = positiveDelta % 7;
-//        if (adjust > 3) {
-//            adjust -= 7;
-//        }
-
-        int adjust = targetDOW - baseDOW;
-        if (adjust >= 4) {
-            adjust = adjust - 7;
+        int distance = targetDOW - baseDOW;
+        if (distance >= 4) {
+            distance = distance - 7;
         }
-        if (adjust <= -4) {
-            adjust = 7 + adjust;
+        if (distance <= -4) {
+            distance = 7 + distance;
         }
 
-//        int adjust = -Math.abs(targetDOW - baseDOW);
-//        if (adjust >= 4) {
-//            adjust = 7 - adjust;
-//        }
-//        if (adjust <= -4) {
-//            adjust = 7 + adjust;
-//        }
-
-        return SerialDate.addDays(adjust, base);
+        return SerialDate.addDays(distance, base);
 
     }
 
